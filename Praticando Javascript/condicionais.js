ef3de7087112d2a403f277401f077dca4ca1c417
LUIZ GUSTAVO DOS SANTOS E SOUZA <55477595892@info.local>
2026-09-15 16:23:33 -0300

    :white_check_mark: Praticando JavaScript

---
+++ Praticando Javascript/condicionais.js
@@ -0,0 +1,90 @@
+//EXERCERCIOS - CONDICIONAIS
+
+//1. 
+// let idade = Number(prompt("Digite sua idade:"));
+
+// if(idade >= 18) {
+//     alert("Você é maior de idade.");
+// }
+// else {
+//     alert("Você é menor de idade.");
+// }
+
+
+//2.
+
+// let numero = Number(prompt("Digite um número:"));
+
+// if ( numero <= 0) {
+//     console.log("O numero é negativo");
+// }
+// else {
+//     console.log("O numero é positivo");
+// }
+
+
+//3.
+
+// let nota = Number(prompt("Digite a nota da prova:"));
+
+// if(nota >= 60 & nota <= 100){
+//     console.log("Aprovado");   
+// }
+// else {
+//     console.log("Reprovado");
+// }
+
+
+
+//4.
+// let numero = Number(prompt("Digite um número :"));
+
+// if ( numero < 0) {
+//     console.log("O numero é negativo");
+// }
+// else if( numero > 0) {
+//     console.log("O numero é positivo");
+// }
+// else {
+//    console.log("O numero é 0");
+   
+// }
+
+
+
+//5.
+
+// let idade = Number(prompt("Digite sua idade:"));
+
+// if( idade > 0 && idade <= 12) {
+//     alert("Criança");
+// }
+// else if( idade  > 12 && idade <=17) {
+//     alert("Adolescente");
+// }
+
+//     else{
+//         alert("Adulto")
+//     }
+
+
+//6.
+let numero = Number(prompt("Digite um número :"));
+
+if( numero % 2 == 0) {
+    alert("O numero é par");
+}
+else {
+    alert("O numero é ímpar");
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Praticando Javascript/repeticao.js
@@ -0,0 +1,50 @@
+//EXERCERCIOS - REPETIÇÃO
+
+//1. contagem de 1 a 10
+// let contagem = 1;
+
+// for (let contagem = 1; contagem <= 10; contagem++) {
+//     console.log(contagem);
+// }
+
+
+//2. Tabuada de um numero
+
+// let numero = Number(prompt("Digite um numero:"));
+// for (let i = 0; i <= 10; i++) {
+//     console.log(numero * i);
+    
+    
+// }
+
+
+//3.Soma dos primeiros N números naturais
+// let numero = Number(prompt("Digite um número N:"));
+
+// let soma = 0;
+
+// for (let i = 1; i <= numero; i++) {
+//     soma + i;
+// }
+// console.log(soma);
+
+
+
+//INTERMEDIARIO
+
+//1. Exibir os números pares de 1 a 50
+
+// let = numero; 
+
+//  for (let numero = 2;  numero<= 50; numero+2) {
+//     console.log(numero);
+    
+    
+//  }
+
+
+
+
+
+
+
